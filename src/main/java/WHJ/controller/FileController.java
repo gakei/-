741ca509bd53370ddb2b7252d93af1ad765c7a84
gakei <584651563@qq.com>
2020-03-17 11:35:59 +0800

    发布问题页支持了Markdown语法

---
+++ src/main/java/WHJ/controller/FileController.java
@@ -0,0 +1,18 @@
+package WHJ.controller;
+
+import WHJ.dto.FileDTO;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class FileController {
+    @RequestMapping("/file/upload")
+    @ResponseBody
+    public FileDTO upload() {
+        FileDTO fileDTO = new FileDTO();
+        fileDTO.setSuccess(1);
+        fileDTO.setUrl("/images/gakei.jfif");
+        return fileDTO;
+    }
+}
